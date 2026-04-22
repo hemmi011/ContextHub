9cdff4eb5a425bf5b3907551c213188254006cff
hemmi011 <now.0311.no1@gmail.com>
2026-04-22 09:54:00 +0900

    typeエラー修正

--- app/test2/page.tsx
+++ app/test2/page.tsx
@@ -1,7 +1,7 @@
-"use clint"
+"use client"
 
 import { supabase } from "@/lib/supabase"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { useState, useEffect } from "react"
 
 type Project = {

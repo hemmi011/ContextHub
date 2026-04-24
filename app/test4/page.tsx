9adcafac27a920b0efd0ec2d9e41c4090e95f04f
hemmi011 <now.0311.no1@gmail.com>
2026-04-24 16:14:18 +0900

    routerをnavigationに変更

--- app/test4/page.tsx
+++ app/test4/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { supabase } from "@/lib/supabase"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 

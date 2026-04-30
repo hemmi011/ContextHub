f0f3544b1ad628d553d1e00809eac66f18deabb2
hemmi011 <now.0311.no1@gmail.com>
2026-04-30 17:51:38 +0900

    test2書き直し

--- app/test2/page.tsx
+++ app/test2/page.tsx
@@ -1,67 +1,94 @@
 "use client"
 
+import { useEffect, useState } from "react"
 import { supabase } from "@/lib/supabase"
 import { useRouter } from "next/navigation"
-import { useState, useEffect } from "react"
+import Link from "next/link"
 
 type Project = {
     id: string
     name: string
-    client_name: string
+    client_nama: string
     status: string
     end_date: string
 }
 
-export default function Home() {
+export default function Home () {
+
     const router = useRouter()
-    const [projectStats, setProjectStats] = useState({active:0, done:0})
-    const [tasksStats, setTasksStats] = useState({todo: 0, inProgress: 0})
+    const [userName, setUserName] = useState("")
+    const [projectStats, setProjectStats] = useState({active: 0, done: 0})
+    const [taskStats, setTaskStatus] = useState({todo: 0, inProgress: 0})
 
     useEffect(() => {
+
         const init = async () => {
+            const {data: getUser}  = await supabase.auth.getUser()
+                if(!getUser.user) {
+                    router.push("/login")
+                    return
+                }
+
             const {data: projects} = await supabase
                 .from("projects")
-                .select("id, name, client_name, status, end_date")
-                .order("created_at", {ascending:false})
+                .select("name, client_name, status, end_date")
+                .order("created_at", {ascending: false})
 
-                if (projects) {
+                if(projects) {
                     setProjectStats({
-                        active: projects.filter((p) => p.status === "進行中").length,
-                        done: projects.filter((p) => p.status === "完了").length,
+                        active: projects.filter((p) => p.status === "進行中").length ,
+                        done: projects.filter((p) => p.status === "完了").length ,
                     })
                 }
 
-                const {data: tasks} = await supabase
-                .from("tasks")
+            const {data: tasks} = await supabase
+                .from("tasks")  
                 .select("id, title, status")
                 .order("created_at", {ascending:false})
 
-            if (tasks) {
-                setTasksStats({
-                    todo: tasks.filter((t) => t.status === "未着手").length,    // (t)は今取り出した１つをtとよぶ。tのstatusが未着手か確認
-                    inProgress: tasks.filter((t) => t.status === "完了").length,
-                })
-            }
 
-        }   
+                if(tasks) {
+                    setTaskStatus({
+                        todo: tasks.filter((t) => t.status === "進行中").length,
+                        inProgress: tasks.filter((t) => t.status === "未着手").length,
+                    })
+                }
+
+                const {data: UserInfo} = await supabase
+                    .from("users")
+                    .select("name")
+                    .eq("id",getUser.user.id).single()
+                    if (UserInfo) setUserName(UserInfo.name)
+            }
 
         init()
     }, [])
 
     return (
-        <div style={{padding: 30, borderRadius: 20, border: "1px solid", backgroundColor: "#b7d2ffff"}}>
-            <p>案件数:{projectStats.active + projectStats.done}</p>
-            <p>進行中：{projectStats.active}</p>
-            <p>完了:{projectStats.done}</p>
+        <>
+            <div style={{padding: 30}}>
 
-            <div>
-            <p>タスク数：{tasksStats.todo + tasksStats.inProgress}件</p>
-            <p>未着手：{tasksStats.todo}</p>
-            <p>完了：{tasksStats.inProgress}</p>
-        </div>
+                <div>
+                    <p>{userName}さん、ようこそ</p>
+                </div>
 
-        </div>
+                <div>
+                    <p>案件数：{projectStats.active + projectStats.done}</p>
+                    <p>進行中：{projectStats.active}</p>
+                    <p>完了：{projectStats.done}</p>
+                </div>
+
+                <div>
+                    <p>タスク数：{taskStats.todo + taskStats.inProgress}</p>
+                    <p>未着手：{taskStats.inProgress}</p>
+                    <p>進行中：{taskStats.todo}</p>
+                </div>
+
+            </div>
+        
+        </>
     )
+
 
 
 }
